530100b08e5a067d708dcc2e95eadbeaf6945b1a
asjadcommon123 <unegbuclinton5@gmail.com>
2023-03-21 03:45:00 +0100

    update screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="mt-[140px] text-center">
+    <div className="mt-[140px] pb-2 text-center">
       <a
-        className="text-lg text-onyx"
+        className="text-sm md:text-lg text-onyx"
         href="https://github.com/unegbuclinton/me"
       >
         Designed and Built by Unegbu Clinton.
