938aa2391d8f4863b1d559ff9a7e5caba5aff703
Clinton <unegbuclinton5@gmail.com>
2023-05-15 10:30:53 +0100

    update link

--- src/utils/work.js
+++ src/utils/work.js
@@ -3,6 +3,14 @@ import image2 from "../assets/images/biggorilla.png";
 import image3 from "../assets/images/aiode.png";
 export const projects = [
   {
+    title: "Aiode",
+    desc: "Aiode is a platform where you can manage and track your health activities at ease.You can now focus on other activities as your health record is seamlesslly tracked",
+    technologies: "React, antd, Redux",
+    img: image3,
+    live: "https://develop--legendary-creponne-6e772f.netlify.app/",
+    repo: "",
+  },
+  {
     title: "FaceChain",
     desc: "This is a media platform that enhance the growth of the crypto community, through, posts, crypto pools, crypto predictions, family pool etc",
     technologies: "React, Tailwind css, Redux",
@@ -17,14 +25,6 @@ export const projects = [
     technologies: "React, Bootstrap, Redux",
     img: image2,
     live: "https://www.biggorillaapps.com/",
-    repo: "",
-  },
-  {
-    title: "Aiode",
-    desc: "Aiode is a platform where you can manage and track your health activities at ease.You can now focus on other activities as your health record is seamlesslly tracked",
-    technologies: "React, antd, Redux",
-    img: image3,
-    live: "https://develop--legendary-creponne-6e772f.netlify.app/",
     repo: "",
   },
 ];
