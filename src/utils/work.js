a7885c66808fe744b9f37bff0fb232ca88c30cb4
Clinton <unegbuclinton5@gmail.com>
2023-08-03 15:27:10 +0100

    update project

--- src/utils/work.js
+++ src/utils/work.js
@@ -1,69 +1,78 @@
-import image1 from "../assets/images/facechain.png";
-import image2 from "../assets/images/biggorilla.png";
-import image3 from "../assets/images/aiode.png";
-import living from "../assets/images/living.png";
-import tictactoe from "../assets/images/tictactoe.png";
-import loruki from "../assets/images/loruki.png";
-import estore from "../assets/images/e-store.png";
+import image1 from '../assets/images/facechain.png'
+import image2 from '../assets/images/biggorilla.png'
+import image3 from '../assets/images/aiode.png'
+import living from '../assets/images/living.png'
+import image4 from '../assets/images/casa.png'
+import tictactoe from '../assets/images/tictactoe.png'
+import loruki from '../assets/images/loruki.png'
+import estore from '../assets/images/e-store.png'
 export const projects = [
   {
-    title: "Aiode",
-    desc: "Aiode is a platform where you can manage and track your health activities at ease.You can now focus on other activities as your health record is seamlesslly tracked",
-    technologies: "REACT, ANTD, REDUX",
-    img: image3,
-    live: "https://develop--legendary-creponne-6e772f.netlify.app/",
-    repo: "",
+    title: 'Casa Colombia',
+    desc: 'Casa colomiba is a web application designed to give buyers and sellers flexibiliity in the real estate sector to buy and sell their properties, with the help of google map buyers can easily sell property location and also request for tour (Built for the colombian market).',
+    technologies: 'REACT, TYPESCRIPT, TAILWIND CSS, REDUX',
+    img: image4,
+    live: 'https://casa-colombia.vercel.app/',
+    repo: '',
   },
   {
-    title: "FaceChain",
-    desc: "This is a media platform that enhance the growth of the crypto community, through, posts, crypto pools, crypto predictions, family pool etc",
-    technologies: "REACT, TAILWIND CSS, REDUX",
+    title: 'Aiode',
+    desc: 'Aiode is a platform where you can manage and track your health activities at ease.You can now focus on other activities as your health record is seamlesslly tracked',
+    technologies: 'REACT, ANTD, REDUX',
+    img: image3,
+    live: 'https://develop--legendary-creponne-6e772f.netlify.app/',
+    repo: '',
+  },
+  {
+    title: 'FaceChain',
+    desc: 'This is a media platform that enhance the growth of the crypto community, through, posts, crypto pools, crypto predictions, family pool etc',
+    technologies: 'REACT, TAILWIND CSS, REDUX',
     img: image1,
-    live: "https://facechain.vercel.app/",
-    repo: "",
+    live: 'https://facechain.vercel.app/',
+    repo: '',
   },
 
   {
-    title: "Biggorillaapps",
-    desc: "Big Gorilla Apps is a leading design and development agency that can bring your ideas to life. Help with ideas and creativity to bring your ideas to life.",
-    technologies: "REACT, BOOTSRAP, REDUX",
+    title: 'Biggorillaapps',
+    desc: 'Big Gorilla Apps is a leading design and development agency that can bring your ideas to life. Help with ideas and creativity to bring your ideas to life.',
+    technologies: 'REACT, BOOTSRAP, REDUX',
     img: image2,
-    live: "https://www.biggorillaapps.com/",
-    repo: "",
+    live: 'https://www.biggorillaapps.com/',
+    repo: '',
   },
-];
+]
 
 export const archivedprojects = [
   {
-    title: "Estore",
-    desc: "This is a simple E-store project, built with react and styled-component, using FakeApi for data population.",
-    technologies: "REACT, STYLED-COMPONENTS, AXIOS, REDUX",
+    title: 'Estore',
+    desc: 'This is a simple E-store project, built with react and styled-component, using FakeApi for data population.',
+    technologies: 'REACT, STYLED-COMPONENTS, AXIOS, REDUX',
     img: estore,
-    live: "https://e-cstore.netlify.app",
-    repo: "https://github.com/unegbuclinton/ecommerce",
+    live: 'https://e-cstore.netlify.app',
+    repo: 'https://github.com/unegbuclinton/ecommerce',
   },
   {
-    title: "Loruki",
-    desc: "This is a responsiive deployment platorm, built using html and css.",
-    technologies: "HTML, CSS",
+    title: 'Loruki',
+    desc: 'This is a responsiive deployment platorm, built using html and css.',
+    technologies: 'HTML, CSS',
     img: loruki,
-    live: "https://bucolic-sawine-7eb98b.netlify.app/",
-    repo: "https://github.com/unegbuclinton/Loruki",
+    live: 'https://bucolic-sawine-7eb98b.netlify.app/',
+    repo: 'https://github.com/unegbuclinton/Loruki',
   },
   {
-    title: "Living the life",
-    desc: "This is a simple multi-page blog webpage, built with just html and css.",
-    technologies: "HTML, CSS",
+    title: 'Living the life',
+    desc: 'This is a simple multi-page blog webpage, built with just html and css.',
+    technologies: 'HTML, CSS',
     img: living,
-    live: "https://lorukiii.netlify.app/",
-    repo: "https://github.com/unegbuclinton/Livingthelife",
+    live: 'https://lorukiii.netlify.app/',
+    repo: 'https://github.com/unegbuclinton/Livingthelife',
   },
   {
-    title: "Tic-Tac-Toe",
-    desc: "This is a simple tic-tac-toe game, built with html,css and vanilla javascript",
-    technologies: "HTML, CSS, JAVASCRIPT",
+    title: 'Tic-Tac-Toe',
+    desc: 'This is a simple tic-tac-toe game, built with html,css and vanilla javascript',
+    technologies: 'HTML, CSS, JAVASCRIPT',
     img: tictactoe,
-    live: "https://mytick-tack-toe.netlify.app/",
-    repo: "https://github.com/unegbuclinton/Tic-Tac-Toe",
+    live: 'https://mytick-tack-toe.netlify.app/',
+    repo: 'https://github.com/unegbuclinton/Tic-Tac-Toe',
   },
-];
+]
