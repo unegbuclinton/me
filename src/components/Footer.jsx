612e9d19ac8e0d12bf816f8d095fb5ec6517d412
Clinton <unegbuclinton5@gmail.com>
2023-09-07 12:46:06 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="mt-[140px] pb-2 text-center">
+    <div className='mt-[140px] pb-2 text-center'>
       <a
-        className="text-sm md:text-lg text-onyx"
-        href="https://github.com/unegbuclinton/me"
+        className='text-[8px] md:text-lg text-onyx'
+        href='https://github.com/unegbuclinton/me'
       >
         Designed and Built by Unegbu Clinton.
       </a>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
