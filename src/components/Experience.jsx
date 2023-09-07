888adc8ba647c71d8280cc9033d389b0707dc0ea
Clinton <unegbuclinton5@gmail.com>
2023-09-07 16:47:43 +0100

    footer

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,32 +1,34 @@
-import React from "react";
-import { biggorilla, curacel, zuri, fiver, Clamp } from "../utils/utils";
-import Tab from "./Tab";
-import WorkTemplate from "./WorkTemplate";
+import React from 'react'
+import { biggorilla, curacel, zuri, fiver, Clamp } from '../utils/utils'
+import Tab from './Tab'
+import WorkTemplate from './WorkTemplate'
 
 const Experience = () => {
   const exp = [
     {
-      title: "Clamp ",
+      title: 'Clamp ',
       component: <WorkTemplate works={Clamp} />,
     },
     {
-      title: "Biggorillaapps ",
+      title: 'Biggorillaapps ',
       component: <WorkTemplate works={biggorilla} />,
     },
-    { title: "Curacel ", component: <WorkTemplate works={curacel} /> },
-    { title: "Zuri ", component: <WorkTemplate works={zuri} /> },
-    { title: "Fiver ", component: <WorkTemplate works={fiver} /> },
-  ];
+    { title: 'Curacel ', component: <WorkTemplate works={curacel} /> },
+    { title: 'Zuri ', component: <WorkTemplate works={zuri} /> },
+    { title: 'Fiver ', component: <WorkTemplate works={fiver} /> },
+  ]
   return (
-    <div id="experience" className="lg:px-24 max-w-[985px] my-0 mx-auto">
-      <h1 className="font-extrabold text-lg lg:text-3xl text-center my-10">
+    <div id='experience' className='lg:px-24 max-w-[985px] my-0 mx-auto'>
+      <h1 className='font-extrabold text-lg lg:text-3xl text-center my-10'>
         Experience
       </h1>
-      <p className="text-sm mb-3 animate-pulse">Scroll right....</p>
+      <p className='block md:hidden text-sm mb-3 animate-pulse'>
+        Scroll right....
+      </p>
       <Tab tabs={exp} />
-      <div className="w-full lg:w-[60%] border-b border-grey my-0 mx-auto pt-16" />
+      <div className='w-full lg:w-[60%] border-b border-grey my-0 mx-auto pt-16' />
     </div>
-  );
-};
+  )
+}
 
-export default Experience;
+export default Experience
